5981e110375fb5b510e10425246295e0f597ba78
emmasteimann <emmasteimann@gmail.com>
2013-07-23 13:40:09 -0500

    Created Beaver and Steve downloader.

---
+++ Readme.md
@@ -0,0 +1,21 @@
+Beaver and Steve Downloader
+=========
+
+Purpose....
+-----------
+I made my original comic downloader for Scary Go Round, but then I wondered if I could expand it to my other favorite web comics, I originally followed up with [Okay Pants](http://web.archive.org/web/20070620013447/http://www.okaypants.com/comic.php) and [KinokoFry](http://kinokofry.com) and got them to work, so I've now expanded it to [Beaver and Steve](http://beaverandsteve.com).
+
+Each chapter is created every 25 comics and loaded into its own `.cbz` file. Which can be used with most comic readers. For Mac, see [Simple Comic](http://dancingtortoise.com/simplecomic/) or [Jomic](http://jomic.sourceforge.net/).
+
+How to use...
+-----------
+A downloader tool for my favorite webcomic.
+
+  - Run `bundle`
+  - Run `bundle exec ruby getBeaverAndSteve.rb`
+  - Enjoy all of Beaver And Steve!
+
+What this does....
+-----------
+
+This downloads the entirety of Beaver and Steve from 2004-2008, when James Turner took and indefinite Beaver and Steve hiatus. :: sniffle ::
---
+++ getBeaverAndSteve.rb
@@ -0,0 +1,87 @@
+require 'mechanize'
+require 'logger'
+require 'pry'
+require 'zip/zip'
+
+class GetBeaverAndSteve
+  attr_accessor :agent, :first_page, :chapter_markers
+
+  def initialize()
+    @agent = Mechanize.new
+    @agent.log = Logger.new "mech.log"
+    @agent.user_agent_alias = 'Mac Safari'
+  end
+
+  def start_download
+    # Start with first page of first Chapter
+    base_url = "http://www.beaverandsteve.com/"
+    page = @agent.get("#{base_url}index.php?comic=1")
+    puts @agent.current_page().uri()
+
+    keep_going = true
+    current_chapter = 1
+    page_cursor = 1
+    while keep_going
+      page = @agent.current_page()
+      if page_cursor % 25 == 0
+        zip_previous_chapter(current_chapter)
+        current_chapter = current_chapter + 1
+        puts "Chapter switched to -> " + current_chapter.to_s
+      end
+
+      comic_url = page.search("//img[contains(@src,'comics')]").first.attributes["src"].to_s
+      image_name = comic_url.split('/')[-1]
+      @agent.get("http://www.beaverandsteve.com/#{comic_url}").save("#{chapter_directory(current_chapter)}/#{image_name}")
+      puts "Currently Downloading: #{current_chapter}"
+      puts "Downloading comic address: #{comic_url}"
+
+      next_link = page.search("//a/img[@id='next']").first
+      if next_link.respond_to? :parent
+        next_link_parent = next_link.parent
+        next_url = next_link_parent.attributes["href"].to_s
+        next_page = @agent.get("#{base_url}#{next_url}")
+      else
+        zip_previous_chapter(current_chapter)
+        keep_going = false
+      end
+      page_cursor = page_cursor + 1
+    end
+  end
+
+  def chapter_directory(chapter_id)
+    "beaverandsteve_comics/bns_chapter_#{chapter_id}"
+  end
+
+  def zip_file_path(chapter_id)
+    directory_name = "saved_comics"
+    unless File.directory?(directory_name)
+      FileUtils.mkdir_p(directory_name)
+    end
+    "#{directory_name}/bns_chapter_#{chapter_id}.cbz"
+  end
+
+  def zip_previous_chapter(chapter_id)
+    directory = chapter_directory(chapter_id)
+    zipfile_name = zip_file_path(chapter_id)
+    if File.exist?(zipfile_name)
+      File.delete(zipfile_name)
+    end
+    Zip::ZipFile.open(zipfile_name, 'w') do |zipfile|
+      Dir["#{directory}/**/**"].reject{|f|f==zipfile_name}.each do |file|
+        zipfile.add(file.sub(directory+'/',''),file)
+      end
+    end
+  end
+
+  class << self
+    def new_download()
+      scary_downloader = self.new()
+      scary_downloader.start_download
+    end
+  end
+
+end
+
+GetBeaverAndSteve.new_download()
+
+exit
